a33ca16d8511a5178bfa579097597097004891b1
JSteinbach <jsteinbach@mailbox.org>
2024-05-28 17:03:51 +0200

    Tried it with IamgeTools..

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon()],
-  site: "https://j-steinbach.github.io",
+  site: "https://jsteinbach.com",
 });
